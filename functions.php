7b49a379a177dcbfd13821bf2fe3b53d76e30ecf
farkoush <hananeh.farkoush@gmail.com>
2025-04-04 00:48:29 +0330

    theme json edits + style button

--- functions.php
+++ functions.php
@@ -17,16 +17,7 @@ define( 'CHILD_THEME_MELOMI_VERSION', '1.0.0' );
  * Enqueue styles
  */
 function child_enqueue_styles() {
-//array('astra-theme-css')
-	wp_enqueue_style( 
-		'grand-sunrise-style', 
-		get_stylesheet_uri()
-	);
-	wp_enqueue_style( 
-		'grand-sunrise-parent-style', 
-		get_parent_theme_file_uri( 'style.css' )
-	);
-	// wp_enqueue_style( 'melomi-theme-css', get_stylesheet_directory_uri() . '/style.css', "", CHILD_THEME_MELOMI_VERSION, 'all' );
+	wp_enqueue_style( 'melomi-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_MELOMI_VERSION, 'all' );
 	wp_enqueue_style('icomoon-style',get_stylesheet_directory_uri() . '/assets/style.css');
 }
 
